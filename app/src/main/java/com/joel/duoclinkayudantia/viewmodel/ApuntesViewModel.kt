a593a7afd7616e77e24664b0177476fbc04b5211
ValeskaSK <va.guardia@duocuc.cl>
2025-12-13 22:50:53 -0300

    chore(apuntes): viewmodel con llamada suspend preparada

--- app/src/main/java/com/joel/duoclinkayudantia/viewmodel/ApuntesViewModel.kt
+++ app/src/main/java/com/joel/duoclinkayudantia/viewmodel/ApuntesViewModel.kt
@@ -1,8 +1,21 @@
 package com.joel.duoclinkayudantia.viewmodel
 
+
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.joel.duoclinkayudantia.model.Apunte
 import com.joel.duoclinkayudantia.repository.ApuntesRepository
+import kotlinx.coroutines.launch
 
 class ApuntesViewModel(
     private val repository: ApuntesRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun obtenerApuntes(onResult: (List<Apunte>) -> Unit) {
+        viewModelScope.launch {
+            val apuntes = repository.obtenerApuntes()
+            onResult(apuntes)
+        }
+    }
+}
+
